341fd81f82512d5865dd049ad2e2330a8705e2e9
lucassantoscrfbezerra <lucassantoscrfbezerra@gmail.com>
2020-02-12 16:17:49 -0300

    ADD: da questão 03

--- aula_03/questao_08/index.php
+++ aula_03/questao_08/index.php
@@ -1,7 +1,7 @@
 <html>
 <head></head>
 <body>
-<h3>Exiba a soma de todos os números até o máximo informado pelo usuário pela URL</h3>
+<h3>Q08 : Exiba a soma de todos os números até o máximo informado pelo usuário pela URL</h3>
 <form action="soma.php" method="get">
   Numero: <input type="text" name="somaAte">
   <input type="submit" value="enviar">

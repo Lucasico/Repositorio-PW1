98087a35d8af942d9b45235995436321f6191adf
lucassantoscrfbezerra <lucassantoscrfbezerra@gmail.com>
2020-02-13 09:59:07 -0300

    ADD: da questão 14

---
+++ aula_03/questao_14/index.php
@@ -0,0 +1,9 @@
+<?php
+//Exiba em ordem decrescente todos os números de 500 até 10;
+echo"<h3>Exiba em ordem decrescente todos os números de 500 até 10</h3>";
+$i = 500;
+while($i >= 10){
+  echo"<h4>Números decrescentes: $i</h4>";
+  $i--;
+}
+?>
